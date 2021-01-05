8d428b0bf05b9ea0966bcb953028435b867feb51
gapyoqq <gapyoqq@gmail.com>
2021-01-05 18:52:36 +0300

    Init commit

---
+++ src/Todolist.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export function Todolist() {
+    return <div>
+        <h3>What to learn</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+        <ul>
+            <li><input type="checkbox" checked={true}/><span>
+                Html&Css
+            </span></li>
+            <li><input type="checkbox" checked={true}/><span>
+                Js
+            </span></li>
+            <li><input type="checkbox" checked={false}/><span>
+                React
+            </span></li>
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
+}
